b07596493e381263f4e39a735f444668578a6493
IanDarwin <>
2010-10-30 16:25:42 +0000

    Organize imports police.

--- src/main/java/session/XaJpaDemoBean.java
+++ src/main/java/session/XaJpaDemoBean.java
@@ -3,14 +3,12 @@ package session;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import domain.Order;
 import domain.Customer;
+import domain.Order;
 
 
 public class XaJpaDemoBean {
