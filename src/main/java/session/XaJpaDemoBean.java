ef0c776d3e20a0db60bcf1e6821447f9ff1cd1a3
IanDarwin <>
2010-11-04 17:21:35 +0000

    Remove unneeded name, add needed @Transaction* annotations. Even on the findCustomer method. EclipseLink is picky (right

--- src/main/java/session/XaJpaDemoBean.java
+++ src/main/java/session/XaJpaDemoBean.java
@@ -1,22 +1,29 @@
 package session;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import domain.Customer;
 import domain.Order;
 
-@Stateless(name="XaJpaDemoBean")
+@Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class XaJpaDemoBean {
 
 	@PersistenceContext(name="customer") EntityManager customerEntityManager;
 	@PersistenceContext(name="orders") EntityManager orderEntityManager;
 	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public Customer findCustomer(int id) {
 		return customerEntityManager.find(Customer.class, id);
 	}
 
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public void saveCustomerOrder(Customer c, Order o, Boolean succeeds) {
 		System.out.println("XaJpaDemoBean.saveCustomerOrder()");
 		try {
