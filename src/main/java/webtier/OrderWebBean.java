2a78a9efac71c393a7a3807d371dacde18d86796
IanDarwin <>
2010-11-04 23:56:23 +0000

    Both methods return String (for now, both "done"). FacesMessages are created, for display on the JSF pages.

--- src/main/java/webtier/OrderWebBean.java
+++ src/main/java/webtier/OrderWebBean.java
@@ -2,9 +2,13 @@ package webtier;
 
 import java.util.Date;
 
+import javax.annotation.Resource;
 import javax.ejb.EJB;
+import javax.ejb.SessionContext;
 import javax.ejb.TransactionManagement;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import session.XaJpaDemoBean;
 import domain.Customer;
@@ -21,23 +25,35 @@ public class OrderWebBean {
 	Customer customer;
 	int orderQuantity;
 	
-	public void saveOrder() {
+	public String saveOrder() {
 		Customer c = ejb.findCustomer(1);
 		Order o = new Order();
 		o.setDate(new Date());
 		o.setQuantity(42);
 		
 		ejb.saveCustomerOrder(c, o, true);
+		
+		final FacesMessage facesMessage = new FacesMessage();
+		facesMessage.setSummary("Added OK");
+		facesMessage.setDetail("Saved order " + c + " " + o);
+		FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+		return "done";
 	}
+	
+	@Resource SessionContext sessionContext;
 
-	public void saveOrderFail() {
+	public String saveOrderFail() {
 		Customer c = ejb.findCustomer(1);
 		
 		Order o2 = new Order();
 		o2.setDate(new Date());
 		o2.setQuantity(100);
 
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("(Simulated failure: setRollBackOnly)"));
+		
+		// sessionContext.setRollbackOnly();
 		ejb.saveCustomerOrder(c, o2, false);
+		return "done"; // NOTREACHED
 	}
 }
 
